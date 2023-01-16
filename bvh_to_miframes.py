9651adb8a08babbc6816e8b1016ef35a333a4b16
CHNCZL <1607163096@qq.com>
2023-01-16 12:08:46 +0800

    start!

---
+++ bvh_to_miframes.py
@@ -0,0 +1,33 @@
+import os
+import json
+body_part = []
+# 缩放倍数
+scale = 1
+
+def read_miframe():
+    with open("template.miframes", "r") as file:
+        MiFileData = file.read()
+    MiDict = json.loads(MiFileData)
+    print(MiDict)
+
+def read_bvh():
+    with open('test.bvh', encoding='utf-8') as file:
+        for line in file:
+            # 以空格切分关键词
+            dataBlock = line.split()
+            # 读取HIERARCHY部分数据
+            if dataBlock[0] == "ROOT" or dataBlock[0] == "JOINT":
+                body_part.append(dataBlock[1])
+            # 读取MOTION部分数据
+            if dataBlock[0] == "Frame":
+                for line in file:
+                    keyframe = line.split()
+                    print("------")
+                    for i in range(len(body_part)):
+                        print(body_part[i], keyframe[6*i: 6*i+6])
+
+if __name__ == "__main__":
+    read_miframe()    
+                    
+   
+
---
+++ dae_fbx_to_bvh-master/dae_fbx_to_bvh.py
@@ -0,0 +1,66 @@
+import bpy
+import os
+
+
+def export_bvh(file):
+    bpy.ops.object.select_all(action='TOGGLE')
+
+    obj = scene.objects.get(bpy.data.armatures[0].name)
+    if obj != None:
+        obj.select_set(state = True)
+        scene.objects.active = obj
+
+    #cont = bpy.context.area.type
+    #print(str(cont))
+
+    frame_start = 1
+    frame_end = 100
+
+    if bpy.data.actions:
+        # get all actions
+        action_list = [action.frame_range for action in bpy.data.actions]
+        frame_end = action_list[-1][-1]
+
+    bpy.ops.export_anim.bvh(filepath=directory + '\\' +
+                            file[:-4] + '.bvh', frame_start=frame_start, frame_end=frame_end)
+
+    bpy.ops.object.select_all(action='TOGGLE')
+    bpy.ops.object.select_all(action='TOGGLE')
+
+    bpy.ops.object.delete(use_global=False)
+    print('Exported:'+file)
+
+
+scene = bpy.context.scene
+
+filepath = bpy.data.filepath
+directory = os.path.dirname(filepath)
+
+for obj in bpy.context.scene.objects:
+    if obj.type == 'ARMATURE':
+        obj.select_set(state = True)
+    else:
+        obj.select_set(state = False)
+bpy.ops.object.delete()
+
+for block in bpy.data.armatures:
+    if block.users == 0:
+        bpy.data.armatures.remove(block)
+
+for block in bpy.data.actions:
+    if block.users == 0:
+        bpy.data.actions.remove(block)
+
+for file in os.listdir(directory):
+    if file.endswith('.dae'):
+        bpy.ops.wm.collada_import(filepath=directory+'\\' + file,
+                                  fix_orientation=True,
+                                  find_chains=True)
+        export_bvh(file)
+
+    elif file.endswith('.fbx'):
+        print(file)
+        bpy.ops.import_scene.fbx(filepath=directory+'\\' + file,
+                                 automatic_bone_orientation=True,
+                                 ignore_leaf_bones=True)
+        export_bvh(file)
